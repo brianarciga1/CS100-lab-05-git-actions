7bf08b0dc24d6f830626d10128fa9a60572350b1
daniel-nis <danielnissan211@gmail.com>
2020-11-10 20:16:26 -0800

    Getting CMake Error

--- tests/pass_tests.hpp
+++ tests/pass_tests.hpp
@@ -4,6 +4,9 @@
 #include "gtest/gtest.h"
 #include "../header/rectangle.hpp"
 
-
+TEST(AreaTest, AreaNonZero) {
+    Rectangle* test = new Rectangle(5,10);
+    EXPECT_EQ(test->area(), 50);
+}
 
 #endif
